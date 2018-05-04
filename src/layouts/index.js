c0fdf5b066709798fea30ca647784979f0bd7253
Adro.Codes <hurricane.interactive@gmail.com>
2018-05-04 16:37:31 +1000

    Project cleanup

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,13 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Navbar from '../components/Navbar'
+// import Navbar from '../components/Navbar'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
     <div>{children()}</div>
   </div>
 )
