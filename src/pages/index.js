e6cdd3cc9a58691506d513a528145c3982e3565b
Adro.Codes <hurricane.interactive@gmail.com>
2018-05-31 20:59:14 +1000

    Homepage component, styling & images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
+import SocialMedia from '../components/SocialMedia'
+
+import bgOption1 from '../img/bg-option1.jpg';
+import bgOption2 from '../img/bg-option2.jpg';
+import bgOption3 from '../img/bg-option3.jpg';
+
+let bgOptions = [bgOption1, bgOption2, bgOption3];
+
 export default class IndexPage extends React.Component {
-  render() {
-    return (
-      <section className="section">
-        HOME
-      </section>
-    )
-  }
+    render() {
+        return (
+            <section className="homepage" style={{
+                'background': `url(${ bgOptions[Math.floor(Math.random() * bgOptions.length)] }) no-repeat center center / cover`
+            }}>
+                <p className="display-text">iCM</p>
+                <h1>FANTASIES AT FIVE<span>am</span><br/>Coming Soon</h1>
+                <SocialMedia />
+            </section>
+        )
+    }
 }
