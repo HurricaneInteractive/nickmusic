15651324ae9276b4284f71a500926bc262d50cdb
Adro.Codes <hurricane.interactive@gmail.com>
2018-05-31 20:58:22 +1000

    Footer component & styling

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <a href="https://www.winkingowl.digital/" title="Go to WinkingOwl Digital">site by WinkingOwl Digital</a>
+        </footer>   
+    )
+}
+
+export default Footer;
